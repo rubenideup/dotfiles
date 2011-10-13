32e57b51698afb40eae3222f87775f324a25ab4f
Moisés Maciá <moises.macia@ideup.com>
2011-10-13 09:48:32 +0200

    added ksshaskpath wrapper to bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,3 +4,9 @@
 if [ $(which git-dude 2>/dev/null) ]; then
   git dude .git-dude/
 fi
+
+# wrapper to handle SSH keys in KDE
+if [ $(which ksshaskpass 2>/dev/null) ]; then
+  export SSH_ASKPASS=/usr/bin/ksshaskpass
+  ssh-add </dev/null
+fi
