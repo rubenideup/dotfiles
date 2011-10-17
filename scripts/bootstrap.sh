508924e6112a05988df8bda607de4cd4cbe05272
Moisés Maciá <moises.macia@ideup.com>
2011-10-17 16:39:49 +0200

    fixed paths

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # start to watch local repositories
 if [ $(which git-dude 2>/dev/null) ]; then
-  git dude .git-dude/
+  git dude ~/.git-dude &>/dev/null
 fi
 
 # wrapper to handle SSH keys in KDE
 if [ $(which ksshaskpass 2>/dev/null) ]; then
   export SSH_ASKPASS=/usr/bin/ksshaskpass
-  ssh-add </dev/null
+  ssh-add < /dev/null &
 fi
